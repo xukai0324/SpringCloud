0d7f54d4cee7a2a1e6bb42462826bf9466ce3378
xukai0324 <719990726@qq.com>
2018-01-23 14:10:02 +0800

    修改：     business/config-server/src/main/resources/config/platform-dev.yml 	修改：     business/platform/platform-rest/src/main/java/com/xk/platform/user/controller/UserController.java 	修改：     business/platform/platform-rest/target/classes/com/xk/platform/user/controller/UserController.class 	修改：     business/ssoserver/sso-server.iml

--- business/platform/platform-rest/src/main/java/com/xk/platform/user/controller/UserController.java
+++ business/platform/platform-rest/src/main/java/com/xk/platform/user/controller/UserController.java
@@ -4,6 +4,7 @@ import com.xk.platform.user.api.UserApi;
 import com.xk.platform.user.dto.UserDto;
 import com.xk.platform.user.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,8 +18,12 @@ public class UserController implements UserApi{
     @Autowired
     private UserService userService;
 
+    @Value("${private.test.name}")
+    private String name;
+
     @Override
     public boolean login(UserDto userDto) throws Exception{
+        System.out.println(name+"--------");
         return userService.login(userDto);
     }
 
